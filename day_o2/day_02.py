d7f2b143006e3e45d5fc136b05ff7aca3ddff27c
Your Name <you@example.com>
2022-12-03 10:09:05 +0100

    Solution to first part complete!

---
+++ day_o2/day_02.py
@@ -0,0 +1,45 @@
+input = "day_o2\day_02.txt"
+
+
+def get_my_score(input):
+    new_round_list = []
+    my_score = 0
+
+    # Read file
+    with open(input) as file:
+        round_list = file.read().split("\n")
+        # Create list for each line
+        for item in round_list:
+            new_round_list.append(item.split(" "))
+
+    # Get my score for each round
+    for round in new_round_list:
+        win = 6
+        draw = 3
+        loss = 0
+        rock = 1
+        paper = 2
+        scissors = 3
+
+        if (round[0] == 'A' and round[1] == 'X'):
+            my_score += draw + rock
+        elif (round[0] == 'A' and round[1] == 'Y'):
+            my_score += win + paper
+        elif (round[0] == 'A' and round[1] == 'Z'):
+            my_score += loss + scissors
+        elif (round[0] == 'B' and round[1] == 'X'):
+            my_score += loss + rock
+        elif (round[0] == 'B' and round[1] == 'Y'):
+            my_score += draw + paper
+        elif (round[0] == 'B' and round[1] == 'Z'):
+            my_score += win + scissors
+        elif (round[0] == 'C' and round[1] == 'X'):
+            my_score += win + rock
+        elif (round[0] == 'C' and round[1] == 'Y'):
+            my_score += loss + paper
+        elif (round[0] == 'C' and round[1] == 'Z'):
+            my_score += draw + scissors
+    print("My score for the first exercise would be:", my_score)
+
+
+get_my_score(input)
