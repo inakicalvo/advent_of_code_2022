6bffaf5c1b17a2e5ff91f9bb27cf586e9e4670ad
Your Name <you@example.com>
2022-12-04 15:54:40 +0100

    Day 3, part 2 completed.

--- day_03/day_03.py
+++ day_03/day_03.py
@@ -5,7 +5,7 @@ def fix_backpacks(input):
     all_backpacks = []
     # Read file
     with open(input) as file:
-        backpacks = file.readlines()
+        backpacks = file.read().splitlines()
 
     priority_score = 0
     priority = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -22,6 +22,28 @@ def fix_backpacks(input):
         priority_score += priority.rfind(char) + 1
 
     print("The sum of the priorities is", priority_score)
+
+    # Second part
+    groups = []
+    priority_score = 0
+
+    # Make groups with three elves per group
+    for i in range(0, len(backpacks), 3):
+        groups.append(backpacks[i:i + 3])
+
+    # Find items in common among the 3 members of each group
+    items_in_common = []
+
+    for group in groups:
+        common_item = list(set.intersection(*map(set, group)))
+        items_in_common.append(common_item)
+    items_in_common = [item for sublist in items_in_common for item in sublist]
+    print(items_in_common)
+
+    for item in items_in_common:
+        priority_score += priority.rfind(item) + 1
+
+    print(priority_score)
 
 
 fix_backpacks(input)
